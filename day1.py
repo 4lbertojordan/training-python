c9dec5af117c4c2b860988316aa3708e48d463d9
Alberto Jordan <alberto.jordan_torrijos@mercedes-benz.com>
2022-12-05 18:24:30 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,26 @@
+# Basic print
+print('Hola "Mercedes-Benz"')
+# Operators
+print('El resultado de 550 + 5 es:', 550 + 5)
+
+
+# Strings
+# Text chain
+print('Hola ' + 'Mercedes-Benz')
+# Text chain multiline
+print("Buenas tardes\nBienvenido a Mercedes-Benz")
+# Text tabulation
+print("Tabulando: " + "Buenas tardes\tBienvenido a Mercedes-Benz")
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
+# Text with special characters
+print("Hello I\'m Alberto Jordan - \\SRE-DevOps/")
+
+
+# inpunts
+# print("¿Cuál es tu nombre?")
+# print("Tu nombre es: " + input())
+
+# print(input("¿Cuál es tu nombre?"))
+print("Tu nombre es: "+input("¿Cuál es tu nombre? ")+" "+input("¿Cuál es tu apellido? "))
---
+++ day1_project.py
@@ -0,0 +1,2 @@
+print("Generador de nombres Random")
+print("Tu nueva marca de cerveza es: "+input("¿Cuál es tu color favorito en inglés? ")+" "+input("¿Cuál es el nombre de tu superhéroe favorito? "))
