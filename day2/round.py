93b2ed05f918d2c2c9445ccf75bf03db2569205f
Alberto Jordan <alberto.jordan_torrijos@mercedes-benz.com>
2022-12-06 20:23:04 +0100

    day2 completed

---
+++ day2/round.py
@@ -0,0 +1,16 @@
+result = round(90/7,2)
+
+print(result)
+print(type(result))
+
+
+result = 90.2
+
+print(round(result))
+
+# root 5 round to 2 decimal places
+result = round(5**(1/2),2)
+print(result)
+
+valor = 5
+print(round(valor**(1/2),2))
